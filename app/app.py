9852af73286edde175147334e34f7d3e2839bc12
PureJZ <jzeng246@gmail.com>
2024-10-29 11:45:40 -0400

    started login and creating db

--- app/app.py
+++ app/app.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template( 'home.html')
 @app.route('/login', methods=['POST'])
-def index():
+def sign_in():
     username = request.cookies.get('username')
     if username:
         return render_template('response.html', username=username, request="Already Logged In")
     return render_template('login.html')
-    
+def past_blogs():
+    return render_template( 'blog.html')
