e42cc77f6a071701d7bcf58ae814e4682dea1589
Jackie Zeng <jzeng246@gmail.com>
2024-10-30 00:52:06 -0400

    Moved app.py code into init added signup with password hash; adds ti db

---
+++ app/ __init__.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import sqlite3
+import os
+from werkzeug.security import generate_password_hash, check_password_hash
+from build_db import setup_database
+app.secret_key = os.random(64)
+def get_db_connection():
+    conn = sqlite3.connect('blog.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/')
+def home():
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        password_hash = generate_password_hash(password)
+
+        conn = get_db_connection()
+        try:
+            conn.execute('INSERT INTO users (username, password_hash) VALUES (?, ?)', (username, password_hash))
+            conn.commit()
+        except sqlite3.IntegrityError:
+            return 'Username already exists'
+        finally:
+            conn.close()
+        return redirect(url_for('login'))
+    return render_template('signup.html')
